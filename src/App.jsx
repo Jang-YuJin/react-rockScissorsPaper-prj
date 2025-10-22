3a201e22165826efe76ac1001d48cae1b0933151
Jang-YuJin <jangyj0912@naver.com>
2025-10-22 13:49:53 +0900

    가위바위보 기능 추가, 리셋 기능 추가, 이긴 횟수/경기 횟수 표시

--- src/App.jsx
+++ src/App.jsx
@@ -26,18 +26,75 @@ const judgeObj = {
 }
 
 function App() {
+  const [judgement, setJudgement] = useState(null);           //승패여부
   const [select, setSelect] = useState(null);                 //사용자 가위바위보
+  const [computerSelect, setComputerSelect] = useState(null); //컴퓨터 가위바위보
+  const [userWinCnt, setUserWinCnt] = useState(0);            //사용자 이긴 횟수
+  const [computerWinCnt, setComputerWinCnt] = useState(0);    //컴퓨터 이긴 횟수
+  const [totCnt, setTotCnt] = useState(0);                    //전체 경기 횟수
 
   const play = (userChoice) => {//가위바위보 하는 함수
     setSelect(choice[userChoice]);//사용자 가위바위보 정함
+    let computer = playComputer();
+    setComputerSelect(computer);//컴퓨터 가위바위보 정함
+    judge(choice[userChoice], computer);//승패여부 판단 함수, 횟수까지 셈
   };
+
+  const playComputer = () => {
+    let itemArray = Object.keys(choice);//객체의 키값만 뽑아서 배열로 반환
+    let computerChoice = Math.floor(Math.random()*itemArray.length)
+
+    return choice[itemArray[computerChoice]];
+  };
+
+  const judge = (userSelect, computerSelect) => {
+    setTotCnt((prev) => prev + 1);
+    if(userSelect.name === computerSelect.name){
+      setJudgement(judgeObj.draw);
+    } else if(userSelect.name === 'Rock'){
+      if(computerSelect.name === 'Scissors'){
+        setJudgement(judgeObj.win);
+        setUserWinCnt((prev) => prev + 1);
+      }else{
+        setJudgement(judgeObj.lose);
+        setComputerWinCnt((prev) => prev + 1);
+      }
+    } else if(userSelect.name === 'Scissors'){
+      if(computerSelect.name === 'Paper'){
+        setJudgement(judgeObj.win);
+        setUserWinCnt((prev) => prev + 1);
+      }else{
+        setJudgement(judgeObj.lose);
+        setComputerWinCnt((prev) => prev + 1);
+      }
+    } else{
+      if(computerSelect.name === 'Rock'){
+        setJudgement(judgeObj.win);
+        setUserWinCnt((prev) => prev + 1);
+      }else{
+        setJudgement(judgeObj.lose);
+        setComputerWinCnt((prev) => prev + 1);
+      }
+    }
+  };
+
+  const resetCnt = () => {
+    setTotCnt(0);
+    setUserWinCnt(0);
+    setComputerWinCnt(0);
+  }
 
   return (
     <div className='main'>
-      <div className='container'>
-        <Box title='당신' select={select}></Box>
-        {/* <Box title='컴퓨터' judgement={judgement} select={computerSelect} cnt={computerWinCnt}></Box> */}
+      <div className='play-cnt'>
+        <div>경기 횟수: {totCnt}</div>
+        <div><button className='reset-button' onClick={() => resetCnt()}>경기 초기화</button></div>
       </div>
+      <div className='container'>
+        <Box title='당신' judgement={judgement} select={select} cnt={userWinCnt}></Box>
+        <Box title='컴퓨터' judgement={judgement} select={computerSelect} cnt={computerWinCnt}></Box>
+      </div>
+      {judgement === null ? <div className='desc'>가위 바위 보 버튼을 눌러주세요!</div> : ''}
       <div className='btn-container'>
         <button className='img-button' onClick={() => play('rock')}><img className='btn-img' src={'/button/rock.png'} alt='바위'></img></button>
         <button className='img-button' onClick={() => play('scissors')}><img className='btn-img' src={'/button/scissors.png'} alt='가위'></img></button>
--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
 const Box = (props) => {
+  let judgement = props.judgement;
+  let judgeBoxClass = props.judgement === null ? '' : (props.judgement == '무승부' ? 'draw-box' : (props.judgement == '승리' ? 'win-box' : 'lose-box'));
+  let judgeFontClass = props.judgement === null ? '' : (props.judgement == '무승부' ? 'draw-font' : (props.judgement == '승리' ? 'win-font' : 'lose-font'));
+  if(props.judgement && props.title === '컴퓨터'){
+    if(props.judgement === '승리'){
+      judgement = '패배';
+      judgeBoxClass = 'lose-box';
+      judgeFontClass = 'lose-font';
+    }else if(props.judgement === '패배'){
+      judgement = '승리';
+      judgeBoxClass = 'win-box';
+      judgeFontClass = 'win-font';
+    }
+  }
 
   return (
-    <div className='box-container'>
+    <div className={`box-container ${judgeBoxClass}`}>
         <div className='title'>{props.title && props.title}</div>
         <img className='img-size' src={props.select === null ? '/img/who.png' : props.select.img} alt='question'></img>
-        <div className='jugdge'>승리</div>
+        <div className='win-cnt'>이긴 횟수: {props.cnt}</div>
+        <div className={`jugdge ${judgeFontClass}`}>{judgement}</div>
     </div>
 
   )
